d7a3512f33e23e3b61975a0bb425c3ed654ce919
mtou <mtou@charougna.com>
2011-09-14 14:25:37 +0200

    Create basic JConfig declaration

---
+++ classes/jconfig.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares JConfig
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/classes/jconfig.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides JConfig
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/classes/jconfig.php
+ */
+class JConfig extends JConfig_Core
+{
+}
---
+++ classes/jconfig/core.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares JConfig_Core
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/classes/jconfig/core.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides JConfig_Core
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/classes/jconfig/core.php
+ */
+abstract class JConfig_Core
+{
+}
