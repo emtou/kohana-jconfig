61ef13a71d69c21a054d31f719d625fc3aa95177
mtou <mtou@charougna.com>
2011-09-15 14:01:18 +0200

    Add JConfig basic functionnalities

--- classes/jconfig/core.php
+++ classes/jconfig/core.php
@@ -32,4 +32,108 @@ defined('SYSPATH') OR die('No direct access allowed.');
  */
 abstract class JConfig_Core
 {
+  protected $_models = array();   /** Loaded models' configurations */
+
+
+  /**
+   * Creates and initialises the JConfig
+   *
+   * Can't be called, the factory() method must be used.
+   *
+   * @return JConfig
+   */
+  protected function __construct()
+  {
+  }
+
+
+  /**
+   * Create a chainable instance of the JConfig class
+   *
+   * @return JConfig
+   */
+  public static function factory()
+  {
+    return new JConfig;
+  }
+
+
+  /**
+   * Generates a list of formo fields' configuration
+   *
+   * @param string      $model_alias   Alias of the model
+   * @param Jelly_Model $model         Model instance
+   * @param array       $field_aliases Aliases of the fields
+   *
+   * @return array Formo fields configuration
+   */
+  public function formo_fields($model_alias, Jelly_Model $model, array $field_aliases)
+  {
+    $this->load($model_alias);
+
+    return $this->_models[$model_alias]->formo_fields($model, $field_aliases);
+  }
+
+
+  /**
+   * Returns an internal model
+   *
+   * @param string $model_alias Alias of the model
+   *
+   * @return JConfig_Model
+   */
+  public function get_model($model_alias)
+  {
+    $this->load($model_alias);
+
+    return $this->_models[$model_alias];
+  }
+
+
+  /**
+   * Initialises a Jelly model from configuration file
+   *
+   * @param string     $model_alias Alias of the model to load
+   * @param Jelly_Meta &$meta       Jelly meta instance
+   *
+   * @return null
+   */
+  public function initialize($model_alias, Jelly_Meta & $meta)
+  {
+    $this->load($model_alias);
+
+    $this->_models[$model_alias]->initialize($meta);
+  }
+
+
+  /**
+   * Load a model from configuration file
+   *
+   * @param string $model_alias Alias of the model to load
+   *
+   * @return null
+   */
+  public function load($model_alias)
+  {
+    if ($this->loaded($model_alias))
+    {
+      return;
+    }
+
+    $this->_models[$model_alias] = JConfig_Model::factory($model_alias);
+  }
+
+
+  /**
+   * Checks if a model is already loaded
+   *
+   * @param string $model_alias Alias of the model to load
+   *
+   * @return bool Model is loaded ?
+   */
+  public function loaded($model_alias)
+  {
+    return isset($this->_models[$model_alias]);
+  }
+
 }
