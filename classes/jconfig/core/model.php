61071019d29d477002eb93efd245ca6d5832d02e
mtou <mtou@charougna.com>
2011-09-15 14:00:59 +0200

    Add Model handling

---
+++ classes/jconfig/core/model.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Declares JConfig_Core_Model
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/classes/jconfig/core/model.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides JConfig_Core_Model
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/classes/jconfig/core/model.php
+ */
+abstract class JConfig_Core_Model
+{
+  protected $_alias  = '';        /** alias of the model */
+  protected $_config = NULL;      /** configuration array */
+  protected $_fields = array();   /** fields' configurations */
+
+
+  /**
+   * Creates and initialises the JConfig_Model
+   *
+   * Can't be called, the factory() method must be used.
+   *
+   * @param string $alias Alias of the model
+   *
+   * @return JConfig_Model
+   */
+  protected function __construct($alias)
+  {
+    $this->_alias = $alias;
+
+    $this->_load();
+  }
+
+
+  /**
+   * Load the model from configuration file
+   *
+   * @return null
+   *
+   * @throws JConfig_Exception Can't load model :alias: configuration file :fname not found
+   */
+  protected function _load()
+  {
+    if ($this->_loaded())
+    {
+      return;
+    }
+
+    $this->_config = Kohana::config('jelly/'.$this->_alias);
+    if ( ! $this->_config)
+    {
+      throw new JConfig_Exception(
+        'Can\'t load model :alias: configuration file :fname not found',
+        array(
+          'alias' => $this->_alias,
+          'fname' => 'jelly/'.$this->_alias,
+        )
+      );
+    }
+
+    foreach ($this->_config['fields'] as $field_alias => $field_config)
+    {
+      $this->_fields[$field_alias] = JConfig_Field::factory($field_alias, $field_config);
+    }
+  }
+
+
+  /**
+   * Checks if the model is already loaded
+   *
+   * @return bool Model is loaded ?
+   */
+  protected function _loaded()
+  {
+    return ($this->_config != NULL);
+  }
+
+
+  /**
+   * Create a chainable instance of the JConfig_Model class
+   *
+   * @param string $alias Alias of the model
+   *
+   * @return JConfig_Model
+   */
+  public static function factory($alias)
+  {
+    return new JConfig_Model($alias);
+  }
+
+
+  /**
+   * Generates a list of formo fields' configuration
+   *
+   * @param Jelly_Model $model         Model instance
+   * @param array       $field_aliases Aliases of the fields
+   *
+   * @return array Formo fields configuration
+   */
+  public function formo_fields($model, array $field_aliases)
+  {
+    $formo_fields = array();
+
+    foreach ($field_aliases as $field_alias)
+    {
+      $formo_fields[] = $this->_fields[$field_alias]->formo($model);
+    }
+
+    return $formo_fields;
+  }
+
+
+  /**
+   * Initialises the Jelly model
+   *
+   * @param Jelly_Meta &$meta Jelly meta instance
+   *
+   * @return null
+   */
+  public function initialize(Jelly_Meta & $meta)
+  {
+    $fields = array();
+
+    foreach ($this->_fields as $field_alias => $field)
+    {
+      $fields[$field_alias] = $field->initialize();
+    }
+
+    $meta->table($this->_config['tablename'])->fields($fields);
+  }
+
+}
---
+++ classes/jconfig/model.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares JConfig_Model
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/classes/jconfig/model.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides JConfig_Model
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/classes/jconfig/model.php
+ */
+class JConfig_Model extends JConfig_Core_Model
+{
+}
