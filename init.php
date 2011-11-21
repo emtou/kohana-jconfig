f0a16f1dbfa8db6ad9d1960415bc609941161432
mtou <mtou@charougna.com>
2011-11-21 10:33:56 +0100

    Include superclosure class definition in init

---
+++ init.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Initialise the JConfig module
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/init.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+// Load SuperClosure
+include Kohana::find_file('vendor', 'superclosure/SuperClosure.class');
