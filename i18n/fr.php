31edf37fd21e0c8c9aa9dfccb39f819f9d2259e8
mtou <mtou@charougna.com>
2012-01-26 10:52:45 +0100

    Add french translation

---
+++ i18n/fr.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Translate strings in french
+ *
+ * PHP version 5
+ *
+ * @group jconfig
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-jconfig/tree/master/i18n/fr.php
+ */
+
+defined('SYSPATH') or die('No direct script access.');
+
+return array(
+  ':field must be unique'         => ':field doit être unique',
+  ':field cannot have this value' => ':field ne peut pas avoir cette valeur',
+);
